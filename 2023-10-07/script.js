f93a340388dbfffa50e0a369891fa23f1981f4df
Fabiola Alves <fabiola.ma.santos@gmail.com>
2023-10-07 16:43:04 -0300

    Add files via upload

---
+++ 2023-10-07/script.js
@@ -0,0 +1,22 @@
+// alert("Hello World!")
+
+// var algumacoisa
+
+// algumacoisa = prompt ("Qual o seu nome?")
+
+// alert (algumacoisa)
+
+// var novacoisa
+
+// novacoisa = confirm ("Está com frio?")
+
+// alert (novacoisa)
+
+var verdadeiro = true
+var falso = false
+
+var nomeCompleto = "Fabiola Maria Alves dos Santos"
+
+var idade = 33
+
+var altura = 1.58 
